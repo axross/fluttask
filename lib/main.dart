18c12441031c11809a5f7bbef57fa6997f814e22
axross <me@axross.io>
2018-12-15 23:36:39 -0800

    BugFix: Tasks are reset when new task adder opens

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
-import 'package:fluttask/with_stateful_widget/task_list_screen.dart';
 import 'package:fluttask/with_scoped_model/task_list_model.dart';
+import 'package:fluttask/with_stateful_widget/task_list_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Fluttask',
-      theme: ThemeData(
-        fontFamily: 'Rubik',
-        primarySwatch: Colors.red,
+    return ScopedModel(
+      model: TaskListModel(),
+      child: MaterialApp(
+        title: 'Fluttask',
+        theme: ThemeData(
+          fontFamily: 'Rubik',
+          primarySwatch: Colors.red,
+        ),
+        home: TaskListScreen(),
       ),
-      home: TaskListScreen(),
-      builder: (context, child) => ScopedModel(
-            model: TaskListModel(),
-            child: child,
-          ),
     );
   }
 }
